e048577135748184382288320145e48a107312bc
lenard <8lenard@gmail.com>
2026-07-15 13:31:54 +0800

    ee

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Invitation_ni_avi">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/invitation_ni_avi/",
+  base: "/Invitation_ni_avi/",
 })
