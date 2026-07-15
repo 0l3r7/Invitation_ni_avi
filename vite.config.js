da024a38b10e39ea589416469fb614af1f685cb5
lenard <8lenard@gmail.com>
2026-07-15 13:24:51 +0800

    as

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/Invitation_ni_avi/",
+  base: "/invitation_ni_avi/",
 })
